603928b764ef11b51c42301bc58283a1a95519af
Riaan Kleinhans <61125752+riaankleinhans@users.noreply.github.com>
2025-06-16 19:03:29 +0100

    remove heading

--- calendar_update/entrypoint.sh
+++ calendar_update/entrypoint.sh
@@ -101,8 +101,7 @@ map(
 </style>
 </head>
 <body>
-    <h1>CNCF Project Calendars</h1>
-    <h2>Main Calendar</h2>
+    <h1>Main Calendar</h1>
     <ul>
         <li><img src="https://lf-master-project-logos-prod.s3.us-east-2.amazonaws.com/cncf.svg" alt="CNCF Logo" width="200" height="200" style="vertical-align: middle; margin-right: 12px; object-fit: contain;"> CNCF Main calendar (<a href="https://zoom-lfx.platform.linuxfoundation.org/meetings/cncf">Project calendar</a>)</li>
     </ul>
